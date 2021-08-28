b9a6556715ee6376eb63d766b36b0d204f49f3d4
Gihan Primesha <gihan@ideahub.lk>
2021-08-28 11:53:35 +0530

    Initial commit

---
+++ public/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>App component</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style scoped>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const webpack = require('webpack'),
+	path = require('path');
+const APP_DIR = path.resolve(__dirname, './public/');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+	entry: {
+		app: './public/app.js',
+	},
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash:6].bundle.js',
+		publicPath: process.env.BASE_URL,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: 'vue-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+		],
+	},
+
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			vue: '@vue/runtime-dom',
+		},
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: APP_DIR + '/views/index.html',
+			filename: './index.html',
+		}),
+		new VueLoaderPlugin(),
+	],
+	devtool: 'source-map',
+	devServer: {
+		// inline: false,
+		port: '8090',
+		// contentBase: APP_DIR,
+		// 	watchContentBase: true,
+		// 	static: 'dist',
+	},
+};
